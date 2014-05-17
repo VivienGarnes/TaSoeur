ef01f1f8211f0743a1d676d61b5c86345d760cdb
Alexis Montagne <alexis.montagne@gmail.com>
2014-05-17 16:43:30 +0200

    Update quality and margin

--- lib/image_creator.rb
+++ lib/image_creator.rb
@@ -1,10 +1,11 @@
 class ImageCreator
   def self.create(pin)
-    file = Tempfile.new(["template_#{self.id.to_s}", 'jpg'], 'tmp', encoding: 'ascii-8bit')
-    file.write(IMGKit.new(render('pins/tasoeur_vgt', pin), quality: 50, width: 500, height: 500).to_jpg)
+    file = Tempfile.new(["template_#{pin.id.to_s}", '.png'], 'tmp', encoding: 'ascii-8bit')
+    file.write(IMGKit.new(render('pins/tasoeur_vgt', pin), quality: 100, width: 500, height: 500).to_png)
     file.flush
     pin.quote = file
     pin.save
+    file.unlink
   end
   def self.render(partial, pin, assigns = {})
     view = ActionView::Base.new(ActionController::Base.view_paths, assigns)
