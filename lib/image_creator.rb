fbb3abf3e1638d6fb91016381edb2dd4a2a7620b
Alexis Montagne <alexis.montagne@gmail.com>
2014-05-17 16:11:22 +0200

    Image size set

--- lib/image_creator.rb
+++ lib/image_creator.rb
@@ -1,12 +1,12 @@
 class ImageCreator
-	def self.create(pin)
-	  Rails.logger.warn render('pins/tasoeur_vgt', pin)
-	  kit = IMGKit.new(render('pins/tasoeur_vgt', pin), :quality => 50)
-	  file = kit.to_file(Rails.root.to_s+'/public/tasoeur.png')
-	  pin.quote = File.open(Rails.root.to_s+'/public/tasoeur.png')
-	  pin.save
-	end
-	def self.render(partial, pin, assigns = {})
+  def self.create(pin)
+    file = Tempfile.new(["template_#{self.id.to_s}", 'jpg'], 'tmp', encoding: 'ascii-8bit')
+    file.write(IMGKit.new(render('pins/tasoeur_vgt', pin), quality: 50, width: 500, height: 500).to_jpg)
+    file.flush
+    pin.quote = file
+    pin.save
+  end
+  def self.render(partial, pin, assigns = {})
     view = ActionView::Base.new(ActionController::Base.view_paths, assigns)
     view.extend ApplicationHelper
     view.render(partial: partial, locals: { pin: pin })
