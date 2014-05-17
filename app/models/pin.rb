dadf19d37658306b0e57d6168d3ddbd472a6a921
Alexis Montagne <alexis.montagne@gmail.com>
2014-05-17 16:06:18 +0200

    Add heroku compatibility

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,11 +1,10 @@
 class Pin < ActiveRecord::Base
-     belongs_to :user
-     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-     has_attached_file :quote
+  belongs_to :user
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :quote
 
-     acts_as_votable
+  acts_as_votable
 
-	#validates :image, presence: true
   validates :description, presence: true
 
 end
